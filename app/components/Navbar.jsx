9dddd56b02c7fdba3cbe9408bda3181b38332200
Kaya Kim <kaya@jordancrown.com>
2025-04-16 20:27:22 -0700

    update mobile menu background color

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
         </div>
 
         {/* mobile menu */}
-        <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white">
+        <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-(--color-blue) text-white transition duration-500 dark:bg-stone-800 dark:text-white">
           <button className='absolute right-6 top-6' onClick={closeMenu}>
             <X />
           </button>
